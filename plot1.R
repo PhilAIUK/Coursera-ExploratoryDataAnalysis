945bd0aa11152c46f7d7781f3597c41a610e0578
Philippe <46968479+PhilAIUK@users.noreply.github.com>
2019-03-25 14:39:46 +0000

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,18 @@
+## R Code file to create Plot 1
+
+## This file includes the following steps:
+## - Read the data
+## - Constructs Plot 1
+## - Create plot1.png file
+
+## Read the data
+
+
+## Construct Plot 1
+
+
+## Create plot1.png file
+
+
+
+## Please look at plot1.png file for the result
