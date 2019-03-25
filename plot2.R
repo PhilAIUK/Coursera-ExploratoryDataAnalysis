903a28786683faad5f7ca277a4226fcbb2a6e660
Philippe <46968479+PhilAIUK@users.noreply.github.com>
2019-03-25 14:40:21 +0000

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,18 @@
+## R Code file to create Plot 2
+
+## This file includes the following steps:
+## - Read the data
+## - Constructs Plot 2
+## - Create plot2.png file
+
+## Read the data
+
+
+## Construct Plot 2
+
+
+## Create plot2.png file
+
+
+
+## Please look at plot2.png file for the result
