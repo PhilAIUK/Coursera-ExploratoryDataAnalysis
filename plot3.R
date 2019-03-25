8bbd4880bdb23757e73cad240284585c6e0e7700
Philippe <46968479+PhilAIUK@users.noreply.github.com>
2019-03-25 14:41:28 +0000

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,18 @@
+## R Code file to create Plot 3
+
+## This file includes the following steps:
+## - Read the data
+## - Constructs Plot 3
+## - Create plot1.png file
+
+## Read the data
+
+
+## Construct Plot 3
+
+
+## Create plot3.png file
+
+
+
+## Please look at plot3.png file for the result
