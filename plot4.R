09c04017d81b46ac17c4b4cbda7ff00556933cb2
Philippe <46968479+PhilAIUK@users.noreply.github.com>
2019-03-25 14:41:55 +0000

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,18 @@
+## R Code file to create Plot 4
+
+## This file includes the following steps:
+## - Read the data
+## - Constructs Plot 4
+## - Create plot1.png file
+
+## Read the data
+
+
+## Construct Plot 4
+
+
+## Create plot4.png file
+
+
+
+## Please look at plot4.png file for the result
